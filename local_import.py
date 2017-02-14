c9996aaa930d823eb3386f9f026408cd666834ad
Bjorn Stange <bjorn248@gmail.com>
2017-02-14 11:26:37 -0500

    checking for existence of fields before adding them
    Also turning meetup topics into findaprotest categories
    
    Resigning with correct gpg key and changing commit author

--- local_import.py
+++ local_import.py
@@ -14,37 +14,36 @@ categories = requests.get('https://api.meetup.com/find/topic_categories?&sign=tr
 print("Getting topics from meetup...")
 topics = requests.get('https://api.meetup.com/find/topics?photo-host=public&query=protest&sign=true&key=' + meetup_api_key).json()
 
-print("Calling FindAProtest API to insert topics")
+print("Calling FindAProtest API to insert topics as categories")
 for topic in topics:
-    response = requests.post('https://findaprotest.herokuapp.com/api/generic/movement',
+    response = requests.post('https://findaprotest.herokuapp.com/api/generic/category',
             data = {
-                'name': topic['name'],
-                'description': topic['description'],
-                'date': int(time.time()),
-                'link': 'https://www.meetup.com/topics/' + topic['urlkey'] + '/'
+                'name': topic['name']
             })
     print(topic['name'])
 
 print("Getting events from meetup...")
+
 events = requests.get('https://api.meetup.com/2/open_events?and_text=False&country=us&offset=0&city=Gainesville&format=json&limited_events=False&state=fl&photo-host=public&page=500&radius=25.0&desc=False&status=upcoming&sign=true&key=' + meetup_api_key).json()
 
 # This currently results in 500s from Dave's API...investigate
 print ("Calling FindAProtest API to insert events")
 for event in events['results']:
+    print(event['venue'] if 'venue' in event else "Unknown")
     response = requests.post('https://findaprotest.herokuapp.com/api/generic/event',
             data = {
                 'name': event['group']['name'],
-                'description': event['description'],
-                'city': event['venue']['city'],
-                'state': event['venue']['state'],
-                'location': event['venue']['name'],
-                'summary': event['name'], # you can thank meetup for this confusing name field
-                'eventTime': event['time'],
-                'createdTime': event['created'],
-                'updatedTime': event['updated'],
-                'link': event['event_url'],
-                'estimatedSize': event['yes_rsvp_count'],
-                'actualSize': event['yes_rsvp_count']
+                'description': event['description'] if 'description' in event else "Unknown",
+                'city': event['venue']['city'] if 'venue' in event and 'city' in event['venue'] else "Gainesville",
+                'state': event['venue']['state'] if 'venue' in event and 'state' in event['venue'] else "FL",
+                'location': event['venue']['name'] if 'venue' in event and 'name' in event['venue'] else "Unknown",
+                'summary': event['name'] if 'name' in event else "Unknown", # you can thank meetup for this confusing name field
+                'eventTime': event['time'] / 1000 if 'time' in event else "Unknown",
+                'createdTime': event['created'] / 1000 if 'created' in event else "Unknown",
+                'updatedTime': event['updated'] / 1000 if 'updated' in event else "Unknown",
+                'link': event['event_url'] if 'event_url' in event else "Unknown",
+                'estimatedSize': event['yes_rsvp_count'] if 'yes_rsvp_count' in event else "Unknown",
+                'actualSize': event['yes_rsvp_count'] if 'yes_rsvp_count' in event else "Unknown"
             })
     print(response.text)
     print(event['group']['name'])
